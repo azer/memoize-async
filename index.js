3a5b62e7ea3c66906cdab26a6132017645c08502
Azer Koçulu <azer@kodfabrik.com>
2014-01-28 15:42:26 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function memoize(fn, hash){
   !hash && ( hash = function(n){ return n; });
 
   return function(){
-
     var args     = Array.prototype.slice.call(arguments),
         key      = hash.apply(undefined, args),
         callback = args.pop();
@@ -24,26 +23,21 @@ function memoize(fn, hash){
     queues[key] = [callback];
 
     args.push(function(error/*,  result */){
-
       !error && ( mem[ key ] = arguments );
 
       run(queues[key], arguments);
 
       delete queues[key];
-
     });
 
     fn.apply(undefined, args);
-
   };
 };
 
 function run(callbacks, args){
-
   var i = callbacks.length;
 
   while( i -- ){
-
     try {
       callbacks[i].apply(undefined, args);
     } catch(err){
@@ -51,7 +45,5 @@ function run(callbacks, args){
         throw err;
       });
     }
-
   }
-
 }
